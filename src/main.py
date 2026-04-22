90da33405b9dddb0075061941fcc5ab0e9daec3d
Roblu01 <roblu.aguilar@gmail.com>
2026-04-21 20:35:43 -0600

    basics fix

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
-from textnode import TextNode
+from textnode import TextNode, TextType
 
 def main():
-    print(TextNode('This is some anchor text', 'link', 'https://www.boot.dev'))
+    print(TextNode('This is some anchor text', TextType.LINK, 'https://www.boot.dev'))
 if __name__ == "__main__":
     main()

8e590df301878de6d691887a0ac64164764a671b
Roblu01 <roblu.aguilar@gmail.com>
2026-05-21 16:12:38 -0600

    markdown to html edits

--- src/markdown_to_html.py
+++ src/markdown_to_html.py
@@ -1,6 +1,6 @@
-from markdown_blocks import markdown_to_blocks,BlockType,block_to_block_type
+from markdown_blocks import BlockType, markdown_to_blocks, block_to_block_type
 from htmlnode import ParentNode
-from textnode import text_node_to_html_node,TextNode,TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 from splitdelimiter import text_to_textnodes
 
 

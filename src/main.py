ea82d0296a921802ef0147dd845e2bbd50c0f6b8
Roblu01 <roblu.aguilar@gmail.com>
2026-05-25 21:56:46 -0600

    more pages

--- src/main.py
+++ src/main.py
@@ -5,7 +5,10 @@ from generate_page import generate_page
 def main():
     copy_static_to_public()
     generate_page('content/index.md','template.html','public/index.html')
-
+    generate_page('content/blog/glorfindel/index.md','template.html','public/blog/glorfindel/index.html')
+    generate_page('content/blog/tom/index.md','template.html','public/blog/tom/index.html')
+    generate_page('content/blog/majesty/index.md','template.html','public/blog/majesty/index.html')
+    generate_page('content/contact/index.md','template.html','public/contact/index.html')
 
 if __name__ == "__main__":
     main()
